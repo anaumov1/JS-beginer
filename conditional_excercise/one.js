361cee5f77bf792f7ff1b87a8dc5885780a8b6d4
AlexN <82428461+anaumov1@users.noreply.github.com>
2021-06-05 00:32:12 -0500

    conditionals if/else

---
+++ conditional_excercise/one.js
@@ -0,0 +1,47 @@
+// Predict what each of the snippets below will print out.
+
+// snippet 1-0
+let qty = 38;
+
+if (qty > 30 && qty % 5 === 4) {
+  console.log("swish");
+} else {
+  console.log("swoosh");               // swoosh
+}
+
+if (qty > 0) {
+  console.log("pos");              // pos
+}
+
+// snippet 1-1
+let a = "celery";
+let b = "SQUASH";
+
+if (a === a.toUpperCase()) {
+  console.log("alpha");
+}
+
+if (b === b.toUpperCase()) {
+  console.log("beta");              // beta
+}
+
+// snippet 1-2
+let number = 9;
+
+if (number > 4) {
+  console.log("ding");            //ding
+} else if (number % 3 === 0) {
+  console.log("dong");            // will not run because it already ran the first if
+}
+
+// snippet 1-3
+let z = 12;
+
+if (z > 10) {
+  console.log("vroom");                 // vroom
+}
+
+if (z % 3 === 0) {
+  console.log("skrrt");                 // skrrt
+}
+
---
+++ conditional_excercise/zero.js
@@ -0,0 +1,61 @@
+// Predict what each of the code snippets below will print out.
+
+// snippet 0-1
+if (true) {
+    console.log("foo");  // foo
+}
+
+if(false) {
+    console.log("bar");  // will not run
+}
+
+// snippet 0-2
+
+if (false || false) {
+    console.log("boop");   // not run
+}
+
+if (true || false) {
+    console.log("beep");   //  beep
+}
+
+// snippet 0-3
+
+let num = 40;
+
+if (num > 0) {
+    console.log("zip")     //zip
+}
+
+if (num % 2 === 0) {
+    console.log("zoop");       //zoop
+  } 
+
+  // snippet 4
+
+  let word = "jeep";
+
+  if (word[0] === "d") {
+    console.log("yer");    // nothing
+  } else {
+    console.log("nah");     // nah
+  }
+
+  //snippet 0-5
+
+  let sentence = "roger that";
+
+if (sentence[sentence.length - 1] === "t") {
+  console.log("ends in t");                      //ends in t
+} else {
+  console.log("does not end in t");
+}
+
+if (sentence.length <= 4) {
+  console.log("short");
+} else {
+  console.log("long");                           // long
+}
+
+
+
