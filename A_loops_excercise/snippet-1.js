6096d927ccefbf3fec144499dc4af6018ba0f4ed
AlexN <82428461+anaumov1@users.noreply.github.com>
2021-07-01 18:21:05 -0500

    loops predicting behaviour excsrsice

---
+++ A_loops_excercise/snippet-1.js
@@ -0,0 +1,11 @@
+// Predict what would print when executing each of the snippets below.
+
+// snippet 1
+console.log("hello");
+
+for(let i = 0; i < 5; i++) {
+    console.log("code");
+}
+
+console.log("goodbye");
+
