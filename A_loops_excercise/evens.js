264ce7e4f44c067bb432295fc084fd0153a472fc
AlexN <82428461+anaumov1@users.noreply.github.com>
2021-07-02 10:55:44 -0500

    added loop excercises

---
+++ A_loops_excercise/evens.js
@@ -0,0 +1,22 @@
+// Write a function `evens(max)` that accepts a max number as an argument. The function should print
+// all positive even numbers that are less than the max.
+
+let evens = function(max) {
+    for (i=2; i < (max); i +=2) {
+        console.log(i);
+    }
+};
+
+evens(11);
+// prints
+//  2
+//  4
+//  6
+//  8
+//  10
+
+evens(8);
+// prints
+//  2
+//  4
+//  6
