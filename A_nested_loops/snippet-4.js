0aaa1003a95a9d379b57d204d86460b02f58536d
AlexN <82428461+anaumov1@users.noreply.github.com>
2021-07-23 17:43:48 -0500

    snippets af nested loops to predict behaviour

---
+++ A_nested_loops/snippet-4.js
@@ -0,0 +1,9 @@
+// snippet 4
+let locations = ["flatbush", "williamsburg", "bushwick", "greenpoint"];
+
+for (let i = 0; i < locations.length; i++) {
+  for (let j = i + 1; j < locations.length; j++) {
+    console.log(locations[i], locations[j]);
+  }
+}
+
